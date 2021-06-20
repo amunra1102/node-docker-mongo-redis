c698fac473cdb8f775fd3928c47d7cf599b725c4
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-21 00:31:57 +0700

    docker with node, mongo & redis

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ data: 'Hello World' });
+});
+
+app.listen(port, console.log(`Server is running on Port: ${port}`));
