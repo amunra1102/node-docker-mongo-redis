1a56e200ee6a2a91352eec050c00d63402a42b79
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-21 06:45:20 +0700

    add mongo

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://devuser:gcsvn123@mongo:27017/?authSource=admin')
+    .then(() => console.log("Successfully connected to Mongo"))
+    .catch(e => console.log(e.message));
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ data: 'Hello World!' });
+    res.json({ data: 'Hello World!!!' });
 });
 
 app.listen(port, console.log(`Server is running on Port: ${port}`));
