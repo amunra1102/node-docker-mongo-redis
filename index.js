42abf95fa9dc9559b5d1d1d4cc1f71c5164ca736
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-21 16:42:33 +0700

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const redis = require('redis');
+const cors = require('cors');
 
 const { PORT, MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config');
 
@@ -40,6 +41,7 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.enable('trust proxy');
+app.use(cors({}));
 app.use(session({
     store: new RedisStore({ client: redisClient }),
     secret: SESSION_SECRET,
